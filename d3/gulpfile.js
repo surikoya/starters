77e35fcfebf008107c6b6abfd04b4f01e49fb081
Suresh Koya <surikoya@yahoo.com>
2015-06-23 21:23:46 -0700

    Adding starter project for d3

---
+++ d3/gulpfile.js
@@ -0,0 +1,49 @@
+// include gulp
+var gulp = require('gulp'); 
+ 
+// include plug-ins
+//Reuired to check the quality of our JavaScript source files using jshint
+var jshint = require('gulp-jshint');
+//Required to only pass through changed files
+var changed = require('gulp-changed');
+var minifyHTML = require('gulp-minify-html');
+var stripDebug = require('gulp-strip-debug');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+var htmlSrc = './src/*.html',
+  htmlDst = './build';
+ 
+// JS hint task
+gulp.task('jshint', function() {
+  gulp.src('.src/scripts/app/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// minify new or changed HTML pages
+gulp.task('htmlpage', function() { 
+  gulp.src(htmlSrc)
+    .pipe(changed(htmlDst))
+    .pipe(minifyHTML())
+    .pipe(gulp.dest(htmlDst));
+});
+
+// JS concat, strip debugging and minify
+gulp.task('scripts', function() {
+  gulp.src(['./src/scripts/app/*.js'])
+    .pipe(concat('main.js'))
+    .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts/app/'));
+});
+
+// JS concat, strip debugging and minify
+gulp.task('copyScripts', function() {
+  gulp.src(['./src/scripts/thirdparty/*.js'])
+    .pipe(gulp.dest('./build/scripts/thirdparty/'));
+});
+
+// default gulp task
+gulp.task('default', ['jshint', 'htmlpage', 'scripts', 'copyScripts'], function() {
+});
---
+++ d3/src/scripts/app/main.js
@@ -0,0 +1,9 @@
+requirejs(["/build/scripts/thirdparty/d3.js"], function(d3) {
+    //This function is called when /scripts/thirdparty/d3.js is loaded.
+	var body = d3.select('body');	
+
+	var dataset = [10,5, 11, 12, 13,22];
+
+	body.selectAll('p').data(dataset).enter().append("p").text(function(d,i){return "Value is " + d;});	
+});
+
